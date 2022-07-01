bb57baf7a4c87e6a267e0e6ed021c72443a3b100
Eunbi Kang <jenabill@naver.com>
2022-07-02 01:50:05 +0900

    Rename: NavLinks component

---
+++ components/SideNav/NavLinks/index.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { List, Item, Anchor } from "./NavLinks.styles";
+import { NAV_LINKS } from "./navLinks";
+
+const NavLinks = ({ closeNav }: { closeNav: () => void }) => {
+  const router = useRouter();
+  return (
+    <List>
+      {NAV_LINKS.map((link) => (
+        <Item key={link.id}>
+          <Link href={link.to}>
+            <Anchor
+              currentPath={router.pathname}
+              pathName={link.to}
+              onClick={closeNav}
+            >
+              {link.icon}
+              {link.label}
+            </Anchor>
+          </Link>
+        </Item>
+      ))}
+    </List>
+  );
+};
+
+export default NavLinks;
