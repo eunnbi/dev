dd857b49044bc7b17ef58a7e08ee7e511f25b2a5
Eunbi Kang <jenabill@naver.com>
2022-07-02 19:51:10 +0900

    Design: change design of KeywordItem component

--- components/Keywords/KeywordItem/KeywordItem.styles.ts
+++ components/Keywords/KeywordItem/KeywordItem.styles.ts
@@ -1,19 +1,95 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { SIDE } from "../keywords";
 
-export const Item = styled.li`
-  box-shadow: 1px 3px 10px 0 rgb(0, 0, 0, 0.2);
+export const Item = styled.li<{ side: string }>`
+  background-color: #6b2188;
+  position: absolute;
   border-radius: 30px;
   text-align: center;
-  padding: 1rem;
+  padding: 1.3rem 1rem;
   cursor: pointer;
-  font-size: 1.2rem;
-  font-weight: bold;
+  font-size: 1.4rem;
+  font-weight: 500;
   transition: all 0.2s ease-in-out;
+
+  color: white;
   &:hover {
     transform: scale(1.2);
   }
+
   @keyframes pop {
   }
+  &:after {
+    content: "";
+    position: absolute;
+    width: 0;
+    height: 0;
+    border: 20px solid transparent;
+    left: 50%;
+    margin-left: -10px;
+  }
+  ${({ side }) =>
+    side === SIDE[0]
+      ? topLeftStyle
+      : side === SIDE[1]
+      ? topRightStyle
+      : side === SIDE[2]
+      ? bottomLeftStyle
+      : bottomRightStyle};
+`;
+
+const topStyle = css`
+  top: -8rem;
+  &:after {
+    bottom: 0;
+    border-top-color: #6b2188;
+    border-bottom: 0;
+    margin-bottom: -20px;
+  }
+`;
+
+const bottomStyle = css`
+  bottom: -8rem;
+  &:after {
+    top: 0;
+    border-bottom-color: #6b2188;
+    border-top: 0;
+    margin-top: -20px;
+  }
+`;
+
+const leftStyle = css`
+  left: -1.8rem;
+  &:after {
+    border-right: 0;
+  }
+`;
+
+const rightStyle = css`
+  right: -1.8rem;
+  &:after {
+    border-left: 0;
+  }
+`;
+
+const topLeftStyle = css`
+  ${topStyle};
+  ${leftStyle};
+`;
+
+const topRightStyle = css`
+  ${topStyle};
+  ${rightStyle};
+`;
+
+const bottomLeftStyle = css`
+  ${bottomStyle};
+  ${leftStyle};
+`;
+
+const bottomRightStyle = css`
+  ${bottomStyle};
+  ${rightStyle};
 `;
 
 export const Content = styled.p`
@@ -28,12 +104,16 @@ export const Wrapper = styled.div<{ show: boolean }>`
   display: ${({ show }) => (show ? "flex" : "none")};
   justify-content: center;
   align-items: center;
-  position: absolute;
+  position: fixed;
   top: 0;
-  left: 0;
+  left: 20rem;
   right: 0;
   bottom: 0;
   background-color: rgba(0, 0, 0, 0.3);
+  z-index: 3;
+  @media screen and (max-width: 1020px) {
+    left: 0;
+  }
 `;
 
 export const Box = styled.div`
