aae58c3aa1916ed96ca87ae2bd1270ef87984a99
Eunbi Kang <jenabill@naver.com>
2022-07-02 22:22:20 +0900

    Feat: SkillsSection component

---
+++ components/Skills/skills.tsx
@@ -0,0 +1,64 @@
+export interface SkillType {
+  id: number;
+  tech: string;
+  percentage: number;
+}
+
+export const SKILLS_LANG = [
+  {
+    id: 1,
+    tech: "JavaScript",
+    percentage: 88,
+  },
+  {
+    id: 2,
+    tech: "TypeScript",
+    percentage: 65,
+  },
+];
+
+export const SKILLS_LIB = [
+  {
+    id: 3,
+    tech: "ReactJS",
+    percentage: 85,
+  },
+  {
+    id: 4,
+    tech: "Redux",
+    percentage: 85,
+  },
+  {
+    id: 5,
+    tech: "Redux Saga",
+    percentage: 60,
+  },
+  {
+    id: 6,
+    tech: "React Query",
+    percentage: 80,
+  },
+  {
+    id: 7,
+    tech: "Styled Components",
+    percentage: 88,
+  },
+  {
+    id: 8,
+    tech: "NextJS",
+    percentage: 50,
+  },
+];
+
+export const SKILLS_TOOLS = [
+  {
+    id: 9,
+    tech: "Git",
+    percentage: 80,
+  },
+  {
+    id: 10,
+    tech: "Github",
+    percentage: 85,
+  },
+];
