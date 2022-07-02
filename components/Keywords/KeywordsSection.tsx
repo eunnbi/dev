c4c0fd35dc20764ff0a0697ac79a44ac4f96324d
Eunbi Kang <jenabill@naver.com>
2022-07-02 19:51:27 +0900

    Add: KeywordSection

---
+++ components/Keywords/KeywordsSection.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import KeywordList from "./KeywordList";
+
+const Section = styled.section`
+  position: relative;
+  margin: 8rem 0 12rem;
+  h1 {
+    font-size: 4.5rem;
+  }
+`;
+
+const KeywordsSection = () => {
+  return (
+    <Section>
+      <h1>Who is Eunnbi??</h1>
+      <KeywordList />
+    </Section>
+  );
+};
+
+export default KeywordsSection;
