ae9f4fc5f6ff874e6ac140f44d26fc86e35d3777
Eunbi Kang <jenabill@naver.com>
2022-07-04 03:59:53 +0900

    Refactor: KeywordsSection component

--- components/Keywords/KeywordsSection.tsx
+++ components/Keywords/KeywordsSection.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import KeywordList from "./KeywordList";
 
 const Section = styled.section`
+  width: 100%;
   h1 {
     font-size: 4.5rem;
   }
@@ -11,7 +12,6 @@ const KeywordsSection = () => {
   return (
     <Section>
       <h1>Who is Eunnbi??</h1>
-      <KeywordList />
     </Section>
   );
 };
