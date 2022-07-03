39a129755340e7dbe2c197508bbb6c8b9cd9c89a
Eunbi Kang <jenabill@naver.com>
2022-07-04 03:43:23 +0900

    Design: CareerItem component

--- components/Career/CareerItem/CareerItem.styles.ts
+++ components/Career/CareerItem/CareerItem.styles.ts
@@ -3,18 +3,23 @@ import styled from "styled-components";
 export const Item = styled.li`
   display: flex;
   align-items: flex-start;
+  margin-top: -1px;
   h3 {
     font-size: 1.3rem;
   }
 `;
 
 export const Period = styled.div`
-  margin-top: 5px;
+  margin-top: 0.1rem;
   padding: 1rem 1rem 0 0;
   font-weight: 500;
   min-width: 10rem;
   text-align: right;
   flex-shrink: 0;
+
+  p {
+    font-size: 1rem;
+  }
   @media ${({ theme }) => theme.device.laptop} {
     min-width: auto;
     width: 6rem;
@@ -25,8 +30,8 @@ export const Period = styled.div`
 export const Content = styled.div`
   margin-top: 1px;
   position: relative;
-  border-left: 1px solid rgba(0, 0, 0, 0.2);
-  padding: 1rem 0 2rem 1rem;
+  padding: 1rem 0 2rem 15px;
+  border-left: 1px solid lightgray;
   p {
     margin-top: 0.5rem;
     font-size: 1.2rem;
