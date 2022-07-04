5a1e5970d6aa9d718fc802b81d47f61d1c14796b
Eunbi Kang <jenabill@naver.com>
2022-07-05 03:56:44 +0900

    Refactor: Project page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,12 +1,20 @@
 import type { NextPage } from "next";
 import CustomHead from "../components/common/CustomHead";
+import Heading from "../components/common/Heading";
+import ProjectList from "../components/Projects/ProjectList";
+import ModalsProvider from "../context/ModalsContext";
 
 const Projects: NextPage = () => {
   return (
     <>
       <CustomHead page="Projects" />
       <main>
-        <h1>Projects</h1>
+        <Heading>Projects</Heading>
+        <section>
+          <ModalsProvider>
+            <ProjectList />
+          </ModalsProvider>
+        </section>
       </main>
     </>
   );
