8b86d5a48eecfa15383697f7078d2049b29ab7f7
Eunbi Kang <jenabill@naver.com>
2022-07-04 22:20:31 +0900

    Refactor: KeywordItem component

--- components/Keywords/KeywordItem/index.tsx
+++ components/Keywords/KeywordItem/index.tsx
@@ -1,36 +1,27 @@
-import React, { useRef } from "react";
-import { Item, Content, Wrapper, Box } from "./KeywordItem.styles";
-import { IoClose } from "react-icons/io5";
-import { useContent } from "./useContent";
+import React from "react";
+import { Item, Content } from "./KeywordItem.styles";
 
 interface KeywordItemProps {
   title: string;
   contents: String[];
   icon: React.ReactNode;
-  side: string;
+  position: string;
 }
 
-const KeywordItem = ({ title, contents, icon, side }: KeywordItemProps) => {
-  const ref = useRef<HTMLLIElement>(null);
-  const { show, handleShowContent, handleCloseContent } = useContent(ref);
+const KeywordItem = ({ title, contents, icon, position }: KeywordItemProps) => {
   return (
-    <>
-      <Item ref={ref} onClick={handleShowContent} side={side}>
+    <Item position={position}>
+      <div>
         {icon}
         {title}
-      </Item>
-      <Wrapper show={show}>
-        <Box>
-          <IoClose onClick={handleCloseContent} />
-          {contents.map((content, index) => (
-            <Content key={index}>
-              <span>✔️</span>
-              <span>{content}</span>
-            </Content>
-          ))}
-        </Box>
-      </Wrapper>
-    </>
+      </div>
+      {contents.map((content, index) => (
+        <Content key={index}>
+          <span>✔️</span>
+          <span>{content}</span>
+        </Content>
+      ))}
+    </Item>
   );
 };
 
