e4396eb9086bab58bc52831b6aad0fd587c6e023
Eunbi Kang <jenabill@naver.com>
2022-07-04 23:52:47 +0900

    Design: global media query style

--- styles/GlobalStyle.tsx
+++ styles/GlobalStyle.tsx
@@ -12,15 +12,15 @@ export const GlobalStyle = createGlobalStyle`
         }
     }
 
-    @media ${({ theme }) => theme.device.mobile}, (max-height: 800px) {
+    @media ${({ theme }) => theme.device.mobile}, (max-height: 600px) {
         html {
             font-size: 13px;
         }
     }
 
-    @media ${({ theme }) => theme.device.smallMobile}, (max-height: 700px) {
+    @media ${({ theme }) => theme.device.smallMobile}, (max-height: 300px) {
         html {
-            font-size: 10px;
+            font-size: 11px;
         }
     }
 `;
