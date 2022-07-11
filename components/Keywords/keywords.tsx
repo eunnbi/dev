81edb4118b1c581e5e2967e092751744f0f0bedb
Eunbi Kang <jenabill@naver.com>
2022-07-11 17:15:50 +0900

    Refactor: keywords constant 수정

--- components/Keywords/keywords.tsx
+++ components/Keywords/keywords.tsx
@@ -10,28 +10,34 @@ export const KEYWORDS = [
     id: 1,
     title: "Frontend",
     contents: [
-      "현재 Frontend 개발을 하고 있으며, ReactJS 라이브러리와 NextJS 프레임워크를 주로 다루고 있습니다.",
-      "최근에는 React Native에 관심이 생겨, 추후에 React Native를 이용하여 앱 개발에 도전할 것입니다.",
+      "Typescript와 함께 ReactJS 라이브러리를 주로 다루고 있습니다.",
+      "최근에는 NextJS 프레임워크를 공부하며 프로젝트를 진행하고 있습니다",
     ],
+    references: [],
     icon: "🖥️  ",
   },
   {
     id: 2,
     title: "User Experience",
     contents: [
-      "사용자 경험이 좋은 웹을 만들려고 노력하고 있습니다.",
-      "프로젝트를 진행할 때 사용자 입장을 우선적으로 생각해 의견을 말하는 편입니다.",
-      "처음 쓰는 앱이라도 평소에 쓰던 것처럼 쉽게 사용할 수 있도록 UX가 좋은 앱을 만들겠습니다.",
+      "사용자 경험이 좋은 웹을 만들기 위해 노력합니다.",
+      "처음 쓰는 앱이라도 평소에 쓰던 것처럼 사용성이 좋은 웹을 만들겠습니다.",
     ],
+    references: [],
     icon: "👥  ",
   },
   {
     id: 3,
     title: "Communication",
     contents: [
-      "프론트엔드 개발자는 기획자, 백엔드 개발자, 디자이너 다양한 분야의 사람들과 협업합니다.",
-      "상대방이 내 분야를 잘 모를 수 있다는 것을 인지하여 원활하게 소통이 이루어지도록 합니다.",
-      "저 또한 상대방의 분야를 잘 모른다는 것을 인정하고 경청합니다.",
+      "협업 과정에서 원활한 의사소통이 이루어지도록 노력합니다.",
+      "경청의 자세를 바탕으로 명확한 의사전달을 중요시합니다.",
+    ],
+    references: [
+      {
+        link: "https://velog.io/@eunnbi/Project-%EB%B0%B1%EC%97%94%EB%93%9C%EC%99%80-%ED%98%91%EC%97%85%ED%95%98%EA%B8%B0#%EF%B8%8F-%EB%AA%85%ED%99%95%ED%95%9C-%EC%9D%98%EC%82%AC%EC%A0%84%EB%8B%AC",
+        name: "velog - 백엔드와 소통하기",
+      },
     ],
     icon: "📢 ",
   },
@@ -39,8 +45,19 @@ export const KEYWORDS = [
     id: 4,
     title: "Question",
     contents: [],
+    references: [],
     icon: "🤔  ",
   },
 ];
 
-export type KeywordType = typeof KEYWORDS[0];
+export interface IKeyword {
+  id: number;
+  title: string;
+  references: Reference[];
+  contents: string[];
+  icon: string;
+}
+interface Reference {
+  link?: string;
+  name?: string;
+}
