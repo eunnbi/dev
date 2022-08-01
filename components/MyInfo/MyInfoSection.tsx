f13e81726b49c4590fafdf77aa324e3367d8baa3
Eunbi Kang <jenabill@naver.com>
2022-08-01 21:21:55 +0900

    Refactor: MyInfoSection component

--- components/MyInfo/MyInfoSection.tsx
+++ components/MyInfo/MyInfoSection.tsx
@@ -1,26 +1,5 @@
-import { MY_INFO } from "./info";
+import { MY_INFO } from "../../constants/info";
 import styled from "styled-components";
-import { icons } from "react-icons";
-
-const Section = styled.section`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
-  max-width: 1000px;
-`;
-
-const Info = styled.div`
-  display: flex;
-  gap: 1rem;
-  a:hover {
-    text-decoration: underline;
-  }
-`;
-
-const Label = styled.p`
-  color: ${({ theme }) => theme.color.violet};
-  font-weight: 500;
-  margin-bottom: 0.5rem;
-`;
 
 const MyInfoSection = () => {
   return (
@@ -42,3 +21,23 @@ const MyInfoSection = () => {
 };
 
 export default MyInfoSection;
+
+const Section = styled.section`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+  max-width: 1000px;
+`;
+
+const Info = styled.div`
+  display: flex;
+  gap: 1rem;
+  a:hover {
+    text-decoration: underline;
+  }
+`;
+
+const Label = styled.p`
+  color: ${({ theme }) => theme.color.violet};
+  font-weight: 500;
+  margin-bottom: 0.5rem;
+`;
