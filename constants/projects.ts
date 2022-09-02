950a7c05c35ff1f4b0522deeaa927cf813e60821
Eunbi Kang <jenabill@naver.com>
2022-09-02 15:15:33 +0900

    refactor: projects 데이터 업데이트

--- constants/projects.ts
+++ constants/projects.ts
@@ -25,12 +25,7 @@ export const PROJECTS = [
   {
     id: 1,
     title: "World Movie 프로젝트",
-    images: [
-      "/images/projects/1.png",
-      "/images/projects/4.png",
-      "/images/projects/2.png",
-      "/images/projects/3.png",
-    ],
+    images: ["1.png", "4.png", "2.png", "3.png"],
     period: "2022.01 (1차 완성), 2022.06 (반응형 디자인 적용)",
     overview: `전 세계 영화 정보 제공 애플리케이션입니다.
     카테고리 별로 (현재 상영중인 영화, 인기 있는 영화, 평점 높은 영화) 영화 정보를 제공합니다.
@@ -61,14 +56,14 @@ export const PROJECTS = [
     id: 2,
     title: "모아모아 프로젝트",
     images: [
-      "/images/projects/10.png",
-      "/images/projects/11.png",
-      "/images/projects/12.png",
-      "/images/projects/13.png",
-      "/images/projects/14.png",
-      "/images/projects/15.png",
-      "/images/projects/16.png",
-      "/images/projects/17.png",
+      "10.png",
+      "11.png",
+      "12.png",
+      "13.png",
+      "14.png",
+      "15.png",
+      "16.png",
+      "17.png",
     ],
     period: "2022.03 ~ 2022.04",
     overview: `자산 관리를 위한 가계부와 커뮤니티 서비스를 제공합니다. 모아모아 가계부는 수익지출 내역을 기록할 수 있을 뿐만 아니라 머니로그를 작성할 수 있습니다.
@@ -101,13 +96,7 @@ export const PROJECTS = [
   {
     id: 3,
     title: "Link Memo 프로젝트",
-    images: [
-      "/images/projects/5.png",
-      "/images/projects/6.png",
-      "/images/projects/7.png",
-      "/images/projects/8.png",
-      "/images/projects/9.png",
-    ],
+    images: ["5.png", "6.png", "7.png", "8.png", "9.png"],
     period: "2022.06 ~ 2022.07",
     overview: `나에게 도움이 되는 자료의 링크 주소나 나중에 볼 링크 주소를 메모 형식으로 저장하고 관리할 수 있는 애플리케이션입니다.
     링크 주소를 나만의 카테고리별로 저장할 수 있고, 검색 기능을 통해 저장된 링크 주소 메모를 쉽게 찾을 수 있습니다.
@@ -165,21 +154,28 @@ export const PROJECTS = [
   {
     id: 5,
     title: "고려대학교 여름 해커톤 (AIMECO)",
-    images: [],
+    images: ["18.png", "19.png", "20.png", "21.png", "22.png", "23.png"],
     period: "2022.08.19 ~ 2022.08.21",
-    overview: ``,
-    feStacks: ["Typescript", "NextJS", "Emotion"],
+    overview: `환경 보호 운동을 하나의 문화로 만들기 위한 서비스입니다. 배지 시스템을 도입하여 작은 실천부터 환경 보호 관련 행사 참여까지 유도하고, 
+    활동을 통해 얻은 배지를 SNS에 공유할 수 있습니다.  Gamification을 통해 선의의 경쟁심을 부추기며 환경 보호 실천에 재미와 성취감을 느낄 수 있습니다. 
+    이뿐만 아니라 뉴스 및 저널 / 도서관에서 최근 환경 관련 이슈와 다양한 환경 보호 실천 방법에 대한 유용한 정보를 제공합니다.
+    이후 피드와 팔로우 기능을 도입하여 전 세계 사람들의 환경 보호 실천을 공유할 수 있는 커뮤니티 플랫폼으로 서비스를 확장할 계획입니다.
+    또한, Eco-friendly한 브랜딩을 원하는 기업 / 기관들과의 행사 공동 주최나 홍보도 진행하여 대체 불가능한 환경 보호 운동 커뮤니티 플랫폼으로 나아갈 것 입니다.`,
+    feStacks: ["Typescript", "NextJS", "Recoil", "Mui", "Emotion"],
     beStacks: ["Supabase"],
     deployStacks: ["Netlify"],
     member: "기획자 1명 / 개발자 4명",
     role: "👉 본 프로젝트에서 개발자 포지션을 맡았습니다.",
     review: {
-      text: "",
-      link: "",
-      linkName: "",
+      text: `3일 동안 zero부터 하나의 서비스를 만드는 귀중한 경험을 했다. 
+      서비스를 만들 때 Specialization과 사용자의 동기부여를 어떻게 할 것인지 방법에 대해 고민하는 것이 중요하는 것을 배웠다. 
+      처음으로 Gamification 요소를 적용한 서비스를 만들어보면서 흥미로웠고, Gamification 요소를 적용한 좋은 웹 서비스에 대해 알아보고 싶다.
+      프론트 개발자로서 처음 서버리스 웹을 만들어보았고, 서버리스 웹 어플리케이션에 대해 궁금해져서 자세히 알아봐야겠다. 최종적으로 18팀 중 5등이라는 좋은 성과를 얻어서 기뼜다. 😁`,
+      link: "https://velog.io/@eunnbi/2022-%EA%B3%A0%EB%A0%A4%EB%8C%80%ED%95%99%EA%B5%90-%EC%97%AC%EB%A6%84-%ED%95%B4%EC%BB%A4%ED%86%A4-%ED%9B%84%EA%B8%B0",
+      linkName: "2022 고려대학교 여름 해커톤 후기",
     },
     github: "https://github.com/woog2roid/devel5pers-connecthon",
     link: "https://devel5per-connecthon.herokuapp.com/",
-    tags: ["3일 해커톤", "서버리스 웹 어플리케이션"],
+    tags: ["zero에서 하나의 서비스 만들기까지", "서버리스 웹 어플리케이션"],
   },
 ];
