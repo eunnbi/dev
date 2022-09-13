3941833d545afd6eb4f3695ede289d0dbdbf74ac
Eunbi Kang <jenabill@naver.com>
2022-09-13 22:16:34 +0900

    refactor: projects page

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,20 +1,30 @@
 import type { NextPage } from "next";
+import { useState } from "react";
+import styled from "styled-components";
 import CustomHead from "../../components/common/CustomHead";
 import Heading from "../../components/common/Heading";
+import ProjectFilter from "../../components/Projects/ProjectFilter";
 import ProjectList from "../../components/Projects/ProjectList";
 
 const ProjectsPage: NextPage = () => {
+  const [selectedId, setSelectedId] = useState(0);
+  const onSelect = (id: number) => setSelectedId(id);
   return (
     <>
       <CustomHead page="Projects" />
-      <main>
+      <Main>
         <Heading>Projects</Heading>
-        <section>
-          <ProjectList />
-        </section>
-      </main>
+        <ProjectFilter selectedId={selectedId} onSelect={onSelect} />
+        <ProjectList selectedId={selectedId} />
+      </Main>
     </>
   );
 };
 
 export default ProjectsPage;
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  gap: 2.5rem;
+`;
