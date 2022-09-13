6a1f96c1418870b06441f50527e6a8337773d50e
Eunbi Kang <jenabill@naver.com>
2022-09-13 22:15:49 +0900

    refactor: ProjectList props 추가

--- components/Projects/ProjectList/index.tsx
+++ components/Projects/ProjectList/index.tsx
@@ -2,17 +2,20 @@ import ProjectItem from "../ProjectItem";
 import styled from "styled-components";
 import { PROJECTS } from "../../../constants/projects";
 
-const List = styled.ul`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(0vw, 500px));
-  gap: 7rem;
-  justify-content: center;
-`;
+interface ProjectListProps {
+  selectedId: number;
+}
 
-const ProjectList = () => {
+const ProjectList = ({ selectedId }: ProjectListProps) => {
+  let projects = PROJECTS;
+  if (selectedId === 1) {
+    projects = projects.filter((project) => project.personal);
+  } else if (selectedId === 2) {
+    projects = projects.filter((project) => !project.personal);
+  }
   return (
     <List>
-      {PROJECTS.map((project) => (
+      {projects.map((project) => (
         <ProjectItem key={project.id} project={project} />
       ))}
     </List>
@@ -20,3 +23,10 @@ const ProjectList = () => {
 };
 
 export default ProjectList;
+
+const List = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(0vw, 500px));
+  gap: 7rem;
+  justify-content: center;
+`;
