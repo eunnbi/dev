7110f404daebc8876ddde628843fe8a6f72a7eaf
Eunbi Kang <jenabill@naver.com>
2022-10-04 22:07:34 +0900

    refactor: home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 import CustomHead from "../components/common/CustomHead";
 import styles from "../styles/Home.module.css";
-import KeywordList from "../components/Keywords/KeywordList";
+import KeywordSlider from "../components/Keywords/KeywordsSlider";
 
 const HomePage: NextPage = () => {
   return (
@@ -9,7 +9,7 @@ const HomePage: NextPage = () => {
       <CustomHead page="Home" />
       <main className={styles.main}>
         <h1>Who is Eunnbi??</h1>
-        <KeywordList />
+        <KeywordSlider />
       </main>
     </>
   );
