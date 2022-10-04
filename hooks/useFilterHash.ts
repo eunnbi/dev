2fb10b14b215f4ca0a34643153d39f015eba7805
Eunbi Kang <jenabill@naver.com>
2022-10-04 22:28:17 +0900

    refactor: useFilterHash가 반환하는 함수 useCallback으로 감싸기

--- hooks/useFilterHash.ts
+++ hooks/useFilterHash.ts
@@ -1,15 +1,21 @@
 import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 export const useFilterHash = (FILTERS: string[], basePath: string) => {
   const router = useRouter();
   const [filter, setFilter] = useState(FILTERS[0]);
-  const selectFilter = (hash: string) => {
-    router.push({
-      pathname: basePath,
-      hash,
-    });
-  };
+  const selectFilter = useCallback((hash: string) => {
+    router.push(
+      {
+        pathname: basePath,
+        hash,
+      },
+      undefined,
+      {
+        shallow: true,
+      }
+    );
+  }, []);
 
   useEffect(() => {
     const hash = router.asPath.split("#")[1];
