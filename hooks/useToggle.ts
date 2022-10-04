9e11a6080bab042edc59bc2e0ba02a458104386c
Eunbi Kang <jenabill@naver.com>
2022-10-04 21:40:25 +0900

    refactor: useToggle에서 반환하는 함수들 useCallback으로 감싸기

--- hooks/useToggle.ts
+++ hooks/useToggle.ts
@@ -1,17 +1,17 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 export const useToggle = (
   initialState: boolean
 ): [boolean, () => void, () => void] => {
   const [toggle, setToggle] = useState(initialState);
 
-  const onToggle = () => {
-    setToggle(!toggle);
-  };
+  const onToggle = useCallback(() => {
+    setToggle((toggle) => !toggle);
+  }, []);
 
-  const onClose = () => {
+  const onClose = useCallback(() => {
     setToggle(false);
-  };
+  }, []);
 
   return [toggle, onToggle, onClose];
 };
