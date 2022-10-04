487e10f06176486418fe3e605af74d912e2bcadb
Eunbi Kang <jenabill@naver.com>
2022-10-04 21:40:59 +0900

    add: useTheme hook

---
+++ hooks/useTheme.ts
@@ -0,0 +1,23 @@
+import { useRecoilState } from "recoil";
+import { useEffect } from "react";
+import { themeState } from "../store/themeState";
+
+const THEME = "theme";
+
+export const useTheme = () => {
+  const [{ isLightTheme }, setIsLightTheme] = useRecoilState(themeState);
+
+  const changeTheme = () => {
+    setIsLightTheme(({ isLightTheme }) => {
+      localStorage.setItem(THEME, JSON.stringify(!isLightTheme));
+      return { isLightTheme: !isLightTheme };
+    });
+  };
+  useEffect(() => {
+    const theme = window.localStorage.getItem(THEME);
+    if (theme !== null) {
+      setIsLightTheme({ isLightTheme: JSON.parse(theme) });
+    }
+  }, []);
+  return { isLightTheme, changeTheme };
+};
