fa2e740c652daa9d4c777cdd5f6696e425449fe0
Eunbi Kang <jenabill@naver.com>
2022-10-04 22:06:58 +0900

    refactor: useSlider hook

--- hooks/useSlider.ts
+++ hooks/useSlider.ts
@@ -1,26 +1,30 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
-export const useSilder = (length: number) => {
+export const useSlider = (length: number) => {
   const [position, setPosition] = useState({
     prev: length - 1,
     current: 0,
   });
 
-  const movePrev = () => {
-    const current = (position.current - 1 + length) % length;
-    setPosition({
-      prev: current === 0 ? length - 1 : current - 1,
-      current,
+  const movePrev = useCallback(() => {
+    setPosition((position) => {
+      const current = (position.current - 1 + length) % length;
+      return {
+        prev: current === 0 ? length - 1 : current - 1,
+        current,
+      };
     });
-  };
+  }, []);
 
-  const moveNext = () => {
-    const current = (position.current + 1) % length;
-    setPosition({
-      prev: current === 0 ? length - 1 : current - 1,
-      current,
+  const moveNext = useCallback(() => {
+    setPosition((position) => {
+      const current = (position.current + 1) % length;
+      return {
+        prev: current === 0 ? length - 1 : current - 1,
+        current,
+      };
     });
-  };
+  }, []);
 
   useEffect(() => {
     let timeId = setInterval(() => moveNext(), 5000);
