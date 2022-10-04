4dc78ab4e8fa54ea11c014afe3e6ea8f06c340ba
Eunbi Kang <jenabill@naver.com>
2022-10-04 22:07:50 +0900

    refactor: _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,42 +1,27 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
-import { ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme } from "../styles/theme";
-import { GlobalStyle } from "../styles/GlobalStyle";
-import { useEffect, useState } from "react";
 import ThemeButton from "../components/ThemeButton";
-
-const THEME = "theme";
+import { RecoilRoot } from "recoil";
+import CustomThemeProvider from "../styles/CustomThemeProvider";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [isLightTheme, setIsLightTheme] = useState(true);
-  const changeTheme = () => {
-    localStorage.setItem(THEME, JSON.stringify(!isLightTheme));
-    setIsLightTheme(!isLightTheme);
-    console.log(!isLightTheme);
-  };
-  useEffect(() => {
-    const theme = window.localStorage.getItem(THEME);
-    if (theme !== null) {
-      setIsLightTheme(JSON.parse(theme));
-    }
-  }, []);
   return (
-    <ThemeProvider theme={isLightTheme ? lightTheme : darkTheme}>
-      <GlobalStyle />
-      {pageProps.id ? (
-        <>
-          <ThemeButton changeTheme={changeTheme} isLightTheme={isLightTheme} />
-          <Component {...pageProps} />
-        </>
-      ) : (
-        <Layout>
-          <ThemeButton changeTheme={changeTheme} isLightTheme={isLightTheme} />
-          <Component {...pageProps} />
-        </Layout>
-      )}
-    </ThemeProvider>
+    <RecoilRoot>
+      <CustomThemeProvider>
+        {pageProps.id ? (
+          <>
+            <ThemeButton />
+            <Component {...pageProps} />
+          </>
+        ) : (
+          <Layout>
+            <ThemeButton />
+            <Component {...pageProps} />
+          </Layout>
+        )}
+      </CustomThemeProvider>
+    </RecoilRoot>
   );
 }
 
