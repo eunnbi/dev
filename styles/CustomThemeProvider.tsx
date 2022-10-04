2e7efd01581a4ef3522142cdd789e903cae1ea52
Eunbi Kang <jenabill@naver.com>
2022-10-04 22:03:48 +0900

    add: CustomThemeProvider component

---
+++ styles/CustomThemeProvider.tsx
@@ -0,0 +1,18 @@
+import { ThemeProvider } from "styled-components";
+import { themeState } from "../store/themeState";
+import { useRecoilValue } from "recoil";
+import { lightTheme, darkTheme } from "./theme";
+import { GlobalStyle } from "./GlobalStyle";
+import React from "react";
+
+const CustomThemeProvider = ({ children }: React.PropsWithChildren) => {
+  const { isLightTheme } = useRecoilValue(themeState);
+  return (
+    <ThemeProvider theme={isLightTheme ? lightTheme : darkTheme}>
+      <GlobalStyle />
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export default CustomThemeProvider;
