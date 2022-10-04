a0c8ae35db0afbaf91629c1664c8b0619d4e7406
Eunbi Kang <jenabill@naver.com>
2022-10-04 22:28:34 +0900

    refactor: projects index page

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -2,20 +2,15 @@ import type { NextPage } from "next";
 import styled from "styled-components";
 import CustomHead from "../../components/common/CustomHead";
 import Heading from "../../components/common/Heading";
-import ProjectFilter from "../../components/Projects/ProjectFilter";
-import ProjectList from "../../components/Projects/ProjectList";
-import { FILTERS } from "../../constants/projects";
-import { useFilterHash } from "../../hooks/useFilterHash";
+import ProjectSection from "../../components/Projects/ProjectSection";
 
 const ProjectsPage: NextPage = () => {
-  const { filter, selectFilter } = useFilterHash(FILTERS, "/projects");
   return (
     <>
       <CustomHead page="Projects" />
       <Main>
         <Heading>Projects</Heading>
-        <ProjectFilter selectedFilter={filter} onSelect={selectFilter} />
-        <ProjectList selectedFilter={filter} />
+        <ProjectSection />
       </Main>
     </>
   );
