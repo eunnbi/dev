a6d5e4adfbfa211331e3b9cbee929b85e72ae868
Eunbi Kang <jenabill@naver.com>
2022-10-11 02:52:29 +0900

    add: SectionHeading component

---
+++ components/common/SectionHeading.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+interface SectionHeadingProps {
+  title: string;
+}
+
+const SectionHeading = ({ title }: SectionHeadingProps) => {
+  return (
+    <Wrapper>
+      <h2>{title}</h2>
+    </Wrapper>
+  );
+};
+
+export default SectionHeading;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 32px;
+  h2 {
+    padding-bottom: 5px;
+    border-bottom: 4px solid #000;
+    font-size: 1.8rem;
+  }
+`;
