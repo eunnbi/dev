7e72f2872b315a93426a9991717026c3fe5b8033
Eunbi Kang <jenabill@naver.com>
2022-10-11 02:57:53 +0900

    refactor: MyInfoSection component

--- components/MyInfo/MyInfoSection.tsx
+++ components/MyInfo/MyInfoSection.tsx
@@ -1,21 +1,29 @@
-import { MY_INFO } from "../../constants/info";
+import { MY_INFO_LINKS } from "../../constants/info";
 import styled from "styled-components";
+import Image from "next/image";
+import { NextImageWrapper } from "../common/NextImageWrapper.styled";
+import KeywordsRotatingText from "../Keywords/KeywordsRotatingText";
+import { Tooltip, IconButton } from "@mui/material";
 
 const MyInfoSection = () => {
   return (
     <Section>
-      {MY_INFO.map((info, index) => (
-        <Info key={index}>
-          <Label>{info.label}</Label>
-          {info.link !== "" ? (
-            <a href={info.link} target="_blank" rel="noreferrer">
-              <p>{info.value}</p>
-            </a>
-          ) : (
-            <p>{info.value}</p>
-          )}
-        </Info>
-      ))}
+      <ImageWrapper>
+        <Image
+          src="/images/profile.png"
+          layout="fill"
+          placeholder="blur"
+          blurDataURL="/images/profile.png"
+        />
+      </ImageWrapper>
+      <KeywordsRotatingText fontSize="2rem" textAlign="center" />
+      <div>
+        {MY_INFO_LINKS.map((link, index) => (
+          <Tooltip key={index} title={link.label}>
+            <IconButton href={link.link}>{link.icon}</IconButton>
+          </Tooltip>
+        ))}
+      </div>
     </Section>
   );
 };
@@ -23,21 +31,13 @@ const MyInfoSection = () => {
 export default MyInfoSection;
 
 const Section = styled.section`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
-  max-width: 1000px;
-`;
-
-const Info = styled.div`
   display: flex;
+  flex-direction: column;
   gap: 1rem;
-  a:hover {
-    text-decoration: underline;
-  }
+  align-items: center;
 `;
 
-const Label = styled.p`
-  color: ${({ theme }) => theme.color.violet};
-  font-weight: 500;
-  margin-bottom: 0.5rem;
+const ImageWrapper = styled(NextImageWrapper)`
+  width: 100px;
+  height: 100px;
 `;
