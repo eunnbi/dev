d83b835eca8646e686fe0437f8731236add81bcb
Eunbi Kang <jenabill@naver.com>
2022-10-11 01:32:38 +0900

    add: NextImageWrapper styled component

---
+++ components/common/NextImageWrapper.styled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+export const NextImageWrapper = styled.div`
+  & > span {
+    position: unset !important;
+  }
+  img {
+    height: auto !important;
+    position: relative !important;
+  }
+`;
