a3959b81bf7686af584fd7cff17a817ad7358770
Eunbi Kang <jenabill@naver.com>
2022-10-11 23:03:00 +0900

    rename constants to data

---
+++ pages/posts/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import CustomHead from "../../components/common/CustomHead";
+
+const PostsPage: NextPage = () => {
+  return (
+    <>
+      <CustomHead page="Posts" />
+      <main></main>
+    </>
+  );
+};
+
+export default PostsPage;
