ceb3ee7e6aedd2d5139849487d6b2cec2f97b060
Eunbi Kang <jenabill@naver.com>
2022-10-11 18:23:13 +0900

    refactor: Layout component

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,30 +1,18 @@
-import SideNav from "./SideNav";
-import NavToggleBtn from "./NavToggleBtn";
 import styled from "styled-components";
-import { useToggle } from "../../hooks/useToggle";
-
-const StyledLayout = styled.div`
-  display: flex;
-  overflow-x: hidden;
-  min-height: 100vh;
-  @media screen and (max-width: 1020px) {
-    background-color: white;
-    display: block;
-    border-radius: 0;
-    width: 100%;
-    min-height: 100vh;
-  }
-`;
+import Header from "./Header";
 
 const Layout = ({ children }: React.PropsWithChildren) => {
-  const [toggle, onToggle, closeNav] = useToggle(false);
   return (
-    <StyledLayout>
-      <SideNav showNav={toggle} closeNav={closeNav} />
+    <Wrapper>
+      <Header />
       {children}
-      <NavToggleBtn showNav={toggle} setShowNav={onToggle} />
-    </StyledLayout>
+      {/*<NavToggleBtn showNav={toggle} setShowNav={onToggle} />*/}
+    </Wrapper>
   );
 };
 
 export default Layout;
+
+const Wrapper = styled.div`
+  padding: 0 15px;
+`;
