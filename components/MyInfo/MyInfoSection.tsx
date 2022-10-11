d62526e5810bbc910fd382c7e3dd294d26d17081
Eunbi Kang <jenabill@naver.com>
2022-10-11 18:16:58 +0900

    refactor: MyInfoSection component

--- components/MyInfo/MyInfoSection.tsx
+++ components/MyInfo/MyInfoSection.tsx
@@ -1,11 +1,22 @@
-import { MY_INFO_LINKS } from "../../constants/info";
 import styled from "styled-components";
 import Image from "next/image";
+import { useCallback } from "react";
+import { MY_INFO_LINKS } from "../../constants/info";
 import { NextImageWrapper } from "../common/NextImageWrapper.styled";
 import KeywordsRotatingText from "../Keywords/KeywordsRotatingText";
 import { Tooltip, IconButton } from "@mui/material";
+import { BsGithub } from "react-icons/bs";
+import { AiOutlineMail } from "react-icons/ai";
 
 const MyInfoSection = () => {
+  const iconPicker = useCallback((type: string) => {
+    switch (type) {
+      case "github":
+        return <BsGithub />;
+      case "email":
+        return <AiOutlineMail />;
+    }
+  }, []);
   return (
     <Section>
       <ImageWrapper>
@@ -19,8 +30,8 @@ const MyInfoSection = () => {
       <KeywordsRotatingText fontSize="2rem" textAlign="center" />
       <div>
         {MY_INFO_LINKS.map((link, index) => (
-          <Tooltip key={index} title={link.label}>
-            <IconButton href={link.link}>{link.icon}</IconButton>
+          <Tooltip key={index} title={link.type}>
+            <IconButton href={link.link}>{iconPicker(link.type)}</IconButton>
           </Tooltip>
         ))}
       </div>
--- components/Skills/SkillsSection/index.tsx
+++ components/Skills/SkillsSection/index.tsx
@@ -1,31 +1,41 @@
-import Heading from "../../common/Heading";
 import {
   SKILLS_LANG,
   SKILLS_LIB,
   SKILLS_TOOLS,
 } from "../../../constants/skills";
 import SkillList from "../SkillList";
+import SectionHeading from "../../common/SectionHeading";
+import styled from "styled-components";
 
 const SkillsSection = () => {
   return (
-    <section>
-      <Heading level={2}>
-        Tech <span>Skills</span>
-      </Heading>
+    <Section>
+      <SectionHeading title="Tech Skills" />
       <article>
-        <Heading level={3}>Langauge</Heading>
+        <h3>Language</h3>
         <SkillList skills={SKILLS_LANG} />
       </article>
       <article>
-        <Heading level={3}>Libarary &amp; Framework</Heading>
+        <h3>Libarary &amp; Framework</h3>
         <SkillList skills={SKILLS_LIB} />
       </article>
       <article>
-        <Heading level={3}>Dev Tools</Heading>
+        <h3>Dev Tools</h3>
         <SkillList skills={SKILLS_TOOLS} />
       </article>
-    </section>
+    </Section>
   );
 };
 
 export default SkillsSection;
+
+const Section = styled.section`
+  margin-bottom: 80px;
+  article {
+    margin-bottom: 50px;
+    h3 {
+      font-size: 1.25rem;
+      margin-bottom: 2rem;
+    }
+  }
+`;
