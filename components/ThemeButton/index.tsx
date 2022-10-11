ca315e79b8d0ab62c791e74192467230781cc3c6
Eunbi Kang <jenabill@naver.com>
2022-10-11 22:18:07 +0900

    refactor: ThemeButton component

--- components/ThemeButton/index.tsx
+++ components/ThemeButton/index.tsx
@@ -1,17 +1,31 @@
 import { IoMoon, IoSunny } from "react-icons/io5";
 import { useTheme } from "../../hooks/useTheme";
-import { Button, Circle } from "./ThemeButton.styles";
+import styled from "styled-components";
 import React from "react";
 
 const ThemeButton = () => {
   const { isLightTheme, changeTheme } = useTheme();
   return (
-    <Button onClick={changeTheme} isLightTheme={isLightTheme}>
-      <Circle isLightTheme={isLightTheme} />
-      <IoSunny />
-      <IoMoon />
+    <Button onClick={changeTheme}>
+      {isLightTheme ? <IoSunny /> : <IoMoon />}
     </Button>
   );
 };
 
 export default React.memo(ThemeButton);
+
+const Button = styled.button`
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+  z-index: 5;
+  border-radius: 50%;
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  background-color: #363f47;
+  svg {
+    font-size: 2rem;
+    color: white;
+  }
+`;
