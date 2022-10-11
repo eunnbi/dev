2d6cfc277b9c5df2056001f0a6ef25203b4d1d50
Eunbi Kang <jenabill@naver.com>
2022-10-11 20:58:02 +0900

    design: CareerSection component

--- components/Career/CareerSection/index.tsx
+++ components/Career/CareerSection/index.tsx
@@ -1,13 +1,20 @@
 import SectionHeading from "../../common/SectionHeading";
 import CareerList from "../CareerList";
+import styled from "styled-components";
 
 const CareerSection = () => {
   return (
-    <section>
+    <Section>
       <SectionHeading title="Career & Experience" />
       <CareerList />
-    </section>
+    </Section>
   );
 };
 
 export default CareerSection;
+
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
