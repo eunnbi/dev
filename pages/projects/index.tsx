c9d76bd2d7b7abaa007993f7b85c7a7894a95ff9
Eunbi Kang <jenabill@naver.com>
2022-10-11 22:33:30 +0900

    refactor: projects page

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,25 +1,16 @@
 import type { NextPage } from "next";
-import styled from "styled-components";
 import CustomHead from "../../components/common/CustomHead";
-import Heading from "../../components/common/Heading";
 import ProjectSection from "../../components/Projects/ProjectSection";
 
 const ProjectsPage: NextPage = () => {
   return (
     <>
       <CustomHead page="Projects" />
-      <Main>
-        <Heading>Projects</Heading>
+      <main>
         <ProjectSection />
-      </Main>
+      </main>
     </>
   );
 };
 
 export default ProjectsPage;
-
-const Main = styled.main`
-  display: flex;
-  flex-direction: column;
-  gap: 2.5rem;
-`;
