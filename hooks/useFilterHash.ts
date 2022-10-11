b916bccda9898221aafe879bb3ec816b39d12068
Eunbi Kang <jenabill@naver.com>
2022-10-11 21:00:37 +0900

    refactor: useFilterHash hook

--- hooks/useFilterHash.ts
+++ hooks/useFilterHash.ts
@@ -3,7 +3,7 @@ import { useState, useEffect, useCallback } from "react";
 
 export const useFilterHash = (FILTERS: string[], basePath: string) => {
   const router = useRouter();
-  const [filter, setFilter] = useState(FILTERS[0]);
+  const [filterIndex, setFilter] = useState(0);
   const selectFilter = useCallback((hash: string) => {
     router.push(
       {
@@ -20,11 +20,11 @@ export const useFilterHash = (FILTERS: string[], basePath: string) => {
   useEffect(() => {
     const hash = router.asPath.split("#")[1];
     if (hash) {
-      setFilter(hash);
+      setFilter(FILTERS.findIndex((element) => element === hash));
     } else {
-      setFilter(FILTERS[0]);
+      setFilter(0);
     }
   }, [router.asPath]);
 
-  return { filter, selectFilter };
+  return { filterIndex, selectFilter };
 };
