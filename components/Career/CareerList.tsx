3f3bce7b36d83a57a37d70fd0fbaf5029d2c1368
Eunbi Kang <jenabill@naver.com>
2022-10-11 21:08:46 +0900

    refactor: CareerList component

---
+++ components/Career/CareerList.tsx
@@ -0,0 +1,18 @@
+import { CAREER } from "../../constants/career";
+import CareerItem from "./CareerItem";
+
+const CareerList = () => {
+  return (
+    <ol>
+      {CAREER.map((career) => (
+        <CareerItem
+          key={career.id}
+          content={career.content}
+          period={career.period}
+        />
+      ))}
+    </ol>
+  );
+};
+
+export default CareerList;
