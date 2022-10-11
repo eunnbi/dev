a2038a6eb4a2811bc0a03f70522f9b28531bd2f5
Eunbi Kang <jenabill@naver.com>
2022-10-11 21:01:28 +0900

    info 데이터 수정

--- constants/info.ts
+++ constants/info.ts
@@ -1,41 +1,10 @@
-const today = new Date();
-const year = today.getFullYear();
-const age = year - 2001 + 1;
-
-export const MY_INFO = [
+export const MY_INFO_LINKS = [
   {
-    label: "Name",
-    value: "강은비 (Eunbi Kang)",
-    link: "",
-  },
-  {
-    label: "Age",
-    value: age.toString(),
-    link: "",
-  },
-  {
-    label: "Address",
-    value: "Suwon-si, Gyeonggi-do",
-    link: "",
-  },
-  {
-    label: "E-mail",
-    value: "jenabill@naver.com",
-    link: "mailto:jenabill@naver.com",
-  },
-  {
-    label: "Phone",
-    value: "010-9605-0109",
-    link: "",
-  },
-  {
-    label: "Github",
-    value: "https://github.com/eunnbi",
+    type: "github",
     link: "https://github.com/eunnbi",
   },
   {
-    label: "Dev Blog",
-    value: "https://velog.io/@eunnbi",
-    link: "https://velog.io/@eunnbi",
+    type: "email",
+    link: "mailto:jenabill@naver.com",
   },
 ];
