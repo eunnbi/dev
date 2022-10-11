1d7d3a8a9bdd5fc3a5f6fe1f84a765580e4ca0e4
Eunbi Kang <jenabill@naver.com>
2022-10-11 22:35:05 +0900

    design: global style 수정

--- styles/GlobalStyle.tsx
+++ styles/GlobalStyle.tsx
@@ -1,10 +1,9 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-    main {
+    body, main {
         background-color: ${({ theme }) => theme.color.bgColor};
         color: ${({ theme }) => theme.color.textColor};
-        transition: all 0.3s ease-in-out;
     }
     ::-webkit-scrollbar {
         width: 5px; /* 세로축 스크롤바 길이 */
@@ -17,25 +16,19 @@ export const GlobalStyle = createGlobalStyle`
         border-radius: 8px;
         background-color: lightgray;
     }
-
-    @media ${({ theme }) => theme.device.laptop}{
-        main {
-            margin-left: 0;
-        }
-    }
     @media ${({ theme }) => theme.device.tablet} {
         html {
             font-size: 15px;
         }
     }
 
-    @media ${({ theme }) => theme.device.mobile}, (max-height: 600px) {
+    @media ${({ theme }) => theme.device.mobile}{
         html {
             font-size: 13px;
         }
     }
 
-    @media ${({ theme }) => theme.device.smallMobile}, (max-height: 300px) {
+    @media ${({ theme }) => theme.device.smallMobile} {
         html {
             font-size: 11px;
         }
