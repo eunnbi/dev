228d7cf6c2f3eee3a9b1cc44da9d9f34b9078b90
Eunbi Kang <jenabill@naver.com>
2022-10-13 00:42:16 +0900

    add: usePostFilter hook

---
+++ components/posts/hooks/usePostFilter.ts
@@ -0,0 +1,34 @@
+import { useRouter } from "next/router";
+import { useEffect, useCallback, useContext } from "react";
+import { useRecoilState } from "recoil";
+import { CategoriesContext } from "../../../context/posts/CategoriesContext";
+import { postFilterIndex } from "../../../store/postFilter";
+
+export const usePostFilter = () => {
+  const router = useRouter();
+  const categories = useContext(CategoriesContext);
+  const [filterIndex, setFilterIndex] = useRecoilState(postFilterIndex);
+  const selectFilter = useCallback((hash: string) => {
+    router.push(
+      {
+        pathname: "/posts",
+        hash,
+      },
+      undefined,
+      {
+        shallow: true,
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    const hash = router.asPath.split("#")[1];
+    if (hash) {
+      setFilterIndex(categories.findIndex((element) => element === hash));
+    } else {
+      setFilterIndex(0);
+    }
+  }, [router.asPath]);
+
+  return { filterIndex, selectFilter };
+};
