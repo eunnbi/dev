72ca29eb76a2e26621c982dffbbc6bc52c04c8d5
Eunbi Kang <jenabill@naver.com>
2022-10-13 01:01:26 +0900

    add: convertDateFormat function

---
+++ lib/date.ts
@@ -0,0 +1,19 @@
+const MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export const convertDateFormat = (postDate: string) => {
+  const [year, month, date] = postDate.split("-");
+  return `${MONTHS[Number(month) - 1]} ${date}, ${year}`;
+};
