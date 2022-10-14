afdf8c8d6ea526ce16d0efc3110796c16bc4af37
Eunbi Kang <jenabill@naver.com>
2022-10-15 00:05:18 +0900

    refactor: PostsPage

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,13 +1,43 @@
-import { NextPage } from "next";
+import { InferGetServerSidePropsType } from "next";
 import CustomHead from "../../components/common/CustomHead";
+import PostsSection from "../../components/posts/PostsSection";
+import { CategoriesContext } from "../../context/posts/CategoriesContext";
+import { PostsContext } from "../../context/posts/PostsContext";
+import { getSortedPostsData } from "../../lib/posts";
 
-const PostsPage: NextPage = () => {
+const PostsPage = ({
+  posts,
+  categories,
+}: InferGetServerSidePropsType<typeof getStaticProps>) => {
+  //console.log(posts, categories);
   return (
     <>
       <CustomHead page="Posts" />
-      <main></main>
+      <main>
+        <CategoriesContext.Provider value={categories}>
+          <PostsContext.Provider value={posts}>
+            <PostsSection />
+          </PostsContext.Provider>
+        </CategoriesContext.Provider>
+      </main>
     </>
   );
 };
 
 export default PostsPage;
+
+export const getStaticProps = async () => {
+  const posts = getSortedPostsData();
+  const categories = posts.map((post) => post.category);
+  return {
+    props: {
+      posts,
+      categories: [
+        "All",
+        ...categories.filter(
+          (category, index) => categories.indexOf(category) === index
+        ),
+      ],
+    },
+  };
+};
