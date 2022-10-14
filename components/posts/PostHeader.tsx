ff41035b44685508d0a64c276b580247717f29c1
Eunbi Kang <jenabill@naver.com>
2022-10-14 23:55:17 +0900

    add: PostHeader component

---
+++ components/posts/PostHeader.tsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import { convertDateFormat } from "../../lib/date";
+import { usePostInfo } from "./hooks/usePostInfo";
+
+const PostHeader = () => {
+  const { title, category, date, emoji } = usePostInfo();
+  return (
+    <Wrapper>
+      <span className="emoji">{emoji}</span>
+
+      <h1>{title}</h1>
+      <div>
+        <p>{convertDateFormat(date)}</p>
+        <span>{category}</span>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default PostHeader;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  padding-bottom: 10px;
+  border-bottom: 1px solid lightgray;
+  h1 {
+    font-size: 2rem;
+    line-height: 1.3;
+  }
+  & > div {
+    display: flex;
+    justify-content: space-between;
+    color: #a8a8a8;
+    line-height: 1.5;
+    font-weight: 500;
+  }
+  .emoji {
+    font-size: 5rem;
+    margin-bottom: 20px;
+  }
+`;
