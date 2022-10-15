e1ec78772f3b71b908fe3683575736715438e6d1
Eunbi Kang <jenabill@naver.com>
2022-10-15 16:11:22 +0900

    refactor: PostSection component

--- components/posts/PostSection.tsx
+++ components/posts/PostSection.tsx
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 import Markdown from "./Markdown";
+import PostFooter from "./PostFooter";
 import PostHeader from "./PostHeader";
+import Utterances from "./Utterances";
 
 const PostSection = () => {
   return (
     <Section>
       <PostHeader />
       <Markdown />
+      <PostFooter />
+      <Utterances />
     </Section>
   );
 };
@@ -17,5 +21,5 @@ const Section = styled.section`
   margin: 30px 0;
   display: flex;
   flex-direction: column;
-  gap: 40px;
+  gap: 60px;
 `;
