4a4e5dd80d962e3f452fdb184a31ebf7dd1bd4be
Eunbi Kang <jenabill@naver.com>
2022-10-16 18:22:14 +0900

    refactor: Utterances component

--- components/posts/Utterances.tsx
+++ components/posts/Utterances.tsx
@@ -1,10 +1,7 @@
-import { useRouter } from "next/router";
-import React, { createRef, useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { useTheme } from "styled-components";
 
 function Utterances() {
-  const router = useRouter();
-  const path = router.query!.slug;
   const rootElm = useRef<HTMLDivElement>(null);
   const isUtterancesLoaded = useRef(false);
   const { name } = useTheme();
@@ -29,6 +26,20 @@ function Utterances() {
     rootElm.current.appendChild(utterances);
     isUtterancesLoaded.current = true;
   }, []);
+
+  useEffect(() => {
+    const iframe =
+      document.querySelector<HTMLIFrameElement>(".utterances-frame");
+    if (!iframe) return;
+
+    iframe.contentWindow?.postMessage(
+      {
+        type: "set-theme",
+        theme: name === "dark" ? "photon-dark" : "github-light",
+      },
+      "https://utteranc.es"
+    );
+  }, [name]);
 
   return <div ref={rootElm} />;
 }
