cecee637c3df9c93f89ef526b2170e840942988b
Eunbi Kang <jenabill@naver.com>
2022-10-16 16:39:27 +0900

    add: PostToc component

--- components/posts/PostSection.tsx
+++ components/posts/PostSection.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import Markdown from "./Markdown";
 import PostFooter from "./PostFooter";
 import PostHeader from "./PostHeader";
+import PostToc from "./PostToc";
 import Utterances from "./Utterances";
 
 const PostSection = () => {
@@ -9,6 +10,7 @@ const PostSection = () => {
     <Section>
       <PostHeader />
       <Markdown />
+      <PostToc />
       <PostFooter />
       <Utterances />
     </Section>
---
+++ components/posts/PostToc.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { usePostInfo } from "./hooks/usePostInfo";
+import Toc from "react-toc";
+import styled from "styled-components";
+
+const PostToc = () => {
+  const { current } = usePostInfo();
+  return <CustomToc markdownText={current.content} lowestHeadingLevel={3} />;
+};
+
+export default PostToc;
+
+const CustomToc = styled(Toc)`
+  position: fixed;
+  top: 90px;
+  right: 60px;
+  font-size: 0.9rem;
+  color: #9e9e9e;
+  font-weight: 500;
+  ul {
+    padding-left: 20px;
+  }
+  li {
+    padding-bottom: 10px;
+  }
+  @media screen and (max-width: 1400px) {
+    display: none;
+  }
+`;
