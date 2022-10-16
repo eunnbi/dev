44823e2758c6bdd7b8f445924276df41213ada27
Eunbi Kang <jenabill@naver.com>
2022-10-16 19:23:52 +0900

    refactor: CustomHead component

--- components/common/CustomHead.tsx
+++ components/common/CustomHead.tsx
@@ -2,12 +2,30 @@ import Head from "next/head";
 
 interface CustomHeadProps {
   page: string;
+  description?: string;
+  image?: string;
 }
 
-const CustomHead = ({ page }: CustomHeadProps) => {
+const CustomHead = ({ page, description, image }: CustomHeadProps) => {
   return (
     <Head>
-      <title>{page} | Eunnbi.dev</title>
+      <title>{page}</title>
+      <meta property="author" content="강은비" />
+      <meta
+        property="description"
+        content={description ? description : "은비의 개발 성장 일지"}
+      />
+      <meta
+        property="og:description"
+        content={description ? description : "은비의 개발 성장 일지"}
+      />
+      <meta property="og:author" content="강은비" />
+      <meta property="og:title" content={page} />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:image"
+        content={image ? `/images/${image}` : "/images/og-image.png"}
+      />
     </Head>
   );
 };
