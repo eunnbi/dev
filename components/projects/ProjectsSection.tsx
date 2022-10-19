27115bbaa5200af55268a04e06f31afdb07d2cb6
Eunbi Kang <jenabill@naver.com>
2022-10-19 23:01:00 +0900

    refactor: ProjectsSection component

--- components/projects/ProjectsSection.tsx
+++ components/projects/ProjectsSection.tsx
@@ -1,9 +1,8 @@
 import Filter from "../common/Filter";
 import ProjectList from "./ProjectList";
-import { useFilterHash } from "../../hooks/useFilterHash";
-import { FILTERS, PROJECTS } from "../../data/projects";
+import { useFilterHash } from "@hooks/useFilterHash";
+import { FILTERS, PROJECTS } from "@data/projects";
 import styled from "styled-components";
-import Heading from "../common/Heading";
 
 const ProjectsSection = () => {
   const { filterIndex, selectFilter } = useFilterHash(FILTERS, "/projects");
@@ -13,15 +12,12 @@ const ProjectsSection = () => {
       : PROJECTS.filter((project) => project.category === FILTERS[filterIndex]);
   return (
     <Section>
-      <Heading title="Projects" />
-      <div>
-        <Filter
-          filters={FILTERS}
-          filterIndex={filterIndex}
-          selectFilter={selectFilter}
-        />
-        <ProjectList projects={projects} />
-      </div>
+      <Filter
+        filters={FILTERS}
+        filterIndex={filterIndex}
+        selectFilter={selectFilter}
+      />
+      <ProjectList projects={projects} />
     </Section>
   );
 };
@@ -29,8 +25,7 @@ const ProjectsSection = () => {
 export default ProjectsSection;
 
 const Section = styled.section`
-  margin: 30px 0;
   display: flex;
   flex-direction: column;
-  gap: 2.5rem;
+  gap: 30px;
 `;
