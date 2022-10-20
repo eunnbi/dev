5232a0b1b2db9d3e089538c510c6d4acddad2f77
Eunbi Kang <jenabill@naver.com>
2022-10-21 00:10:01 +0900

    refactor: PostsSection component

--- components/posts/PostsSection.tsx
+++ components/posts/PostsSection.tsx
@@ -2,20 +2,13 @@ import PostFilter from "./PostFilter";
 import PostList from "./PostList";
 import styled from "styled-components";
 import { usePosts } from "./hooks/usePosts";
-import Heading from "@components/common/Heading";
 
 const PostsSection = () => {
   const posts = usePosts();
   return (
     <Section>
-      <div>
-        <Heading title="Posts" />
-        <p>{posts.length} posts</p>
-      </div>
-      <div>
-        <PostFilter />
-        <PostList posts={posts} />
-      </div>
+      <PostFilter />
+      <PostList posts={posts} />
     </Section>
   );
 };
@@ -25,16 +18,6 @@ export default PostsSection;
 const Section = styled.section`
   display: flex;
   flex-direction: column;
-  margin: 30px 0;
-  gap: 2.5rem;
-  & > div:first-child {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 10px;
-    p {
-      font-weight: 500;
-      font-size: 1.1rem;
-    }
-  }
+  margin-top: 2.5rem;
+  gap: 20px;
 `;
