338de49e489be4eca5d1ad81ece1524ee8b737fa
Eunbi Kang <jenabill@naver.com>
2022-10-20 23:36:07 +0900

    refactor: use h2 tag instead of p tag

--- components/keywords/KeywordsRotatingText.tsx
+++ components/keywords/KeywordsRotatingText.tsx
@@ -24,7 +24,7 @@ const KeywordsRotatingText = ({
 
 export default KeywordsRotatingText;
 
-const Text = styled.p<{ fontSize: string; textAlign: string }>`
+const Text = styled.h2<{ fontSize: string; textAlign: string }>`
   font-size: ${({ fontSize }) => fontSize};
   text-align: ${({ textAlign }) => textAlign};
   line-height: 1.2;
