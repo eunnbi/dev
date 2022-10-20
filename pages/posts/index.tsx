2dd6fb2da20c3099185da9d20c5413fc5a369b5a
Eunbi Kang <jenabill@naver.com>
2022-10-21 00:09:47 +0900

    refactor: posts page

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,9 +1,11 @@
 import { InferGetServerSidePropsType } from "next";
+import Heading from "@components/common/Heading";
 import CustomHead from "@components/common/CustomHead";
 import PostsSection from "@components/posts/PostsSection";
 import { CategoriesContext } from "@context/posts/CategoriesContext";
 import { PostsContext } from "@context/posts/PostsContext";
 import { getSortedPostsData } from "@lib/posts";
+import styled from "styled-components";
 
 const PostsPage = ({
   posts,
@@ -13,18 +15,32 @@ const PostsPage = ({
   return (
     <>
       <CustomHead page="Posts" />
-      <main>
+      <Main>
+        <Heading title="Posts" />
+        <p>{posts.length} posts</p>
         <CategoriesContext.Provider value={categories}>
           <PostsContext.Provider value={posts}>
             <PostsSection />
           </PostsContext.Provider>
         </CategoriesContext.Provider>
-      </main>
+      </Main>
     </>
   );
 };
 
 export default PostsPage;
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  margin: 30px 0;
+  & > p {
+    text-align: center;
+    font-weight: 500;
+    font-size: 1.1rem;
+    margin-top: 10px;
+  }
+`;
 
 export const getStaticProps = async () => {
   const posts = getSortedPostsData();
