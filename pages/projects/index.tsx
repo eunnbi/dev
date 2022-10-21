35c0b81b022d07a37e8ec73b6d6fcee8ce610c21
Eunbi Kang <jenabill@naver.com>
2022-10-21 22:41:02 +0900

    refactor: projects page

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,16 +1,28 @@
-import type { NextPage } from "next";
+import type { InferGetStaticPropsType } from "next";
 import CustomHead from "@components/common/CustomHead";
 import Heading from "@components/common/Heading";
 import ProjectsSection from "@components/projects/ProjectsSection";
 import styled from "styled-components";
+import { getSortedProjectsData } from "@lib/projects";
+import { CategoriesContext } from "@context/projects/CategoriesContext";
+import { ProjectsContext } from "@context/projects/ProjectsContext";
+import ProjectFilter from "@components/projects/ProjectFilter";
 
-const ProjectsPage: NextPage = () => {
+const ProjectsPage = ({
+  projects,
+  categories
+}: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <>
       <CustomHead page="Projects" />
       <Main>
         <Heading title="Projects" />
-        <ProjectsSection />
+        <CategoriesContext.Provider value={categories}>
+          <ProjectsContext.Provider value={projects}>
+            <ProjectFilter />
+            <ProjectsSection />
+          </ProjectsContext.Provider>
+        </CategoriesContext.Provider>
       </Main>
     </>
   );
@@ -19,8 +31,21 @@ const ProjectsPage: NextPage = () => {
 export default ProjectsPage;
 
 const Main = styled.main`
-  margin: 30px 0;
+  margin: 30px 0 60px;
   display: flex;
   flex-direction: column;
-  gap: 2.5rem;
+  gap: 3rem;
 `;
+
+export const getStaticProps = async () => {
+  const projects = getSortedProjectsData();
+  const categories = ["All", ...projects.map(project => project.category)];
+  return {
+    props: {
+      projects,
+      categories: categories.filter(
+        (category, index) => categories.indexOf(category) === index
+      )
+    }
+  };
+};
