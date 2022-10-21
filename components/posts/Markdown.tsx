8118dd7cf1a2e312bfc26cc86449c22562a0e7eb
Eunbi Kang <jenabill@naver.com>
2022-10-21 22:29:19 +0900

    refactor: Markdown component

--- components/posts/Markdown.tsx
+++ components/posts/Markdown.tsx
@@ -4,52 +4,54 @@ import rehypeRaw from "rehype-raw";
 import rehypeSlug from "rehype-slug";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
-import { usePostInfo } from "./hooks/usePostInfo";
+import { usePostInfo } from "@hooks/usePostInfo";
 import { useTheme } from "styled-components";
 
 const Markdown = () => {
   const { current } = usePostInfo();
   const { name } = useTheme();
   return (
-    <ReactMarkdown
-      className={
-        name === "light" ? "markdown-body light" : "markdown-body dark"
-      }
-      remarkPlugins={[remarkGfm]}
-      rehypePlugins={[rehypeRaw, rehypeSlug]}
-      transformImageUri={(src) => `/images/posts/${src}`}
-      components={{
-        code({ node, inline, className, children, ...props }) {
-          const match = /language-(\w+)/.exec(className || "");
-          return !inline && match ? (
-            <SyntaxHighlighter
-              children={String(children).replace(/\n$/, "")}
-              style={vscDarkPlus}
-              language={match[1]}
-              PreTag="div"
-            />
-          ) : (
-            <code className={className} {...props}>
-              {children}
-            </code>
-          );
-        },
-        h1({ children }) {
-          const id = String(children[0]).replaceAll(" ", "-").toLowerCase();
-          return <h1 id={id}>{children}</h1>;
-        },
-        h2({ children }) {
-          const id = String(children[0]).replaceAll(" ", "-").toLowerCase();
-          return <h2 id={id}>{children}</h2>;
-        },
-        h3({ children }) {
-          const id = String(children[0]).replaceAll(" ", "-").toLowerCase();
-          return <h3 id={id}>{children}</h3>;
-        },
-      }}
-    >
-      {current.content}
-    </ReactMarkdown>
+    <section>
+      <ReactMarkdown
+        className={
+          name === "light" ? "markdown-body light" : "markdown-body dark"
+        }
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw, rehypeSlug]}
+        transformImageUri={src => `/images/posts/${src}`}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+                children={String(children).replace(/\n$/, "")}
+                style={vscDarkPlus}
+                language={match[1]}
+                PreTag="div"
+              />
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+          h1({ children }) {
+            const id = String(children[0]).replaceAll(" ", "-").toLowerCase();
+            return <h1 id={id}>{children}</h1>;
+          },
+          h2({ children }) {
+            const id = String(children[0]).replaceAll(" ", "-").toLowerCase();
+            return <h2 id={id}>{children}</h2>;
+          },
+          h3({ children }) {
+            const id = String(children[0]).replaceAll(" ", "-").toLowerCase();
+            return <h3 id={id}>{children}</h3>;
+          }
+        }}
+      >
+        {current.content}
+      </ReactMarkdown>
+    </section>
   );
 };
 
