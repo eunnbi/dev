5592edac01eea601a0f5b1adc63599874dd97bfe
Eunbi Kang <jenabill@naver.com>
2022-10-21 22:37:54 +0900

    add: ProjectFilter component

---
+++ components/projects/ProjectFilter.tsx
@@ -0,0 +1,19 @@
+import { useContext } from "react";
+import Filter from "@components/common/Filter";
+import { CategoriesContext } from "@context/projects/CategoriesContext";
+import { useFilterIndex, useChangeFilter } from "@hooks/useFilter";
+
+const ProjectFilter = () => {
+  const categories = useContext(CategoriesContext);
+  const filterIndex = useFilterIndex(categories);
+  const selectFilter = useChangeFilter("/projects");
+  return (
+    <Filter
+      filters={categories}
+      filterIndex={filterIndex}
+      selectFilter={selectFilter}
+    />
+  );
+};
+
+export default ProjectFilter;
