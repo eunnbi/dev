89c8ac37db6fd5d748b1ba5e671bf76275eeb11e
Eunbi Kang <jenabill@naver.com>
2022-11-05 21:46:51 +0900

    refactor: usePosts hook

--- hooks/usePosts.ts
+++ hooks/usePosts.ts
@@ -1,12 +1,13 @@
 import { useContext } from "react";
 import { CategoriesContext } from "@contexts/posts/CategoriesContext";
 import { PostsContext } from "@contexts/posts/PostsContext";
-import { useFilterIndex } from "./useFilter";
+import { useRecoilValue } from "recoil";
+import { postFilterState } from "@stores/postFilterState";
 
 export const usePosts = () => {
   const categories = useContext(CategoriesContext);
   const posts = useContext(PostsContext);
-  const filterIndex = useFilterIndex(categories);
+  const filterIndex = useRecoilValue(postFilterState);
   return filterIndex === 0
     ? posts
     : posts.filter(post => post.category === categories[filterIndex]);
