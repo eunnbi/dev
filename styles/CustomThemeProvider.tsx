9f706f07b78ba6ff8198cd0a2d16560c4d61e6cd
Eunbi Kang <jenabill@naver.com>
2022-11-05 14:44:51 +0900

    refactor: CustomThemeProvider

--- styles/CustomThemeProvider.tsx
+++ styles/CustomThemeProvider.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from "styled-components";
 import { themeState } from "@stores/themeState";
 import { useRecoilValue } from "recoil";
 import { lightTheme, darkTheme } from "./theme";
-import { GlobalStyle } from "./GlobalStyle";
+import { createGlobalStyle } from "styled-components";
 import React from "react";
 
 const CustomThemeProvider = ({ children }: React.PropsWithChildren) => {
@@ -14,5 +14,40 @@ const CustomThemeProvider = ({ children }: React.PropsWithChildren) => {
     </ThemeProvider>
   );
 };
+
+const GlobalStyle = createGlobalStyle`
+body, main {
+    background-color: ${({ theme }) => theme.color.bgColor};
+    color: ${({ theme }) => theme.color.textColor};
+}
+::-webkit-scrollbar {
+    width: 5px; /* 세로축 스크롤바 길이 */
+    height: 5px; /* 가로축 스크롤바 길이 */
+}
+::-webkit-scrollbar-track-piece {
+    background-color: ${({ theme }) => theme.color.bgColor};
+}
+::-webkit-scrollbar-thumb {
+    border-radius: 8px;
+    background-color: lightgray;
+}
+@media ${({ theme }) => theme.device.tablet} {
+    html {
+        font-size: 15px;
+    }
+}
+
+@media ${({ theme }) => theme.device.mobile}{
+    html {
+        font-size: 13px;
+    }
+}
+
+@media ${({ theme }) => theme.device.smallMobile} {
+    html {
+        font-size: 11px;
+    }
+}
+`;
 
 export default CustomThemeProvider;
