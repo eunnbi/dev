a0cff0458cff2d7bdff7f6279b2cc6ca1ec913aa
Eunbi Kang <jenabill@naver.com>
2022-11-05 21:47:50 +0900

    refactor: ProjectFilter component

--- components/projects/ProjectFilter.tsx
+++ components/projects/ProjectFilter.tsx
@@ -1,17 +1,19 @@
 import { useContext } from "react";
 import Filter from "@components/common/Filter";
 import { CategoriesContext } from "@contexts/projects/CategoriesContext";
-import { useFilterIndex, useChangeFilter } from "@hooks/useFilter";
+import { useSetRecoilState } from "recoil";
+import { projectFilterState } from "@stores/projectFilterState";
+import { useProjectFilterIndex } from "@hooks/useFilterIndex";
 
 const ProjectFilter = () => {
   const categories = useContext(CategoriesContext);
-  const filterIndex = useFilterIndex(categories);
-  const selectFilter = useChangeFilter("/projects");
+  const filterIndex = useProjectFilterIndex();
+  const setFilterIndex = useSetRecoilState(projectFilterState);
   return (
     <Filter
       filters={categories}
       filterIndex={filterIndex}
-      selectFilter={selectFilter}
+      setFilterIndex={setFilterIndex}
     />
   );
 };
