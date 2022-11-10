93e38f39f9ccb7a6357149cc6f8ea1f9b149499b
Eunbi Kang <jenabill@naver.com>
2022-11-11 00:00:02 +0900

    refactor: Notice component

--- components/posts/Notice.tsx
+++ components/posts/Notice.tsx
@@ -1,22 +1,44 @@
 import styled from "styled-components";
+import { IoClose } from "react-icons/io5";
+import { useState } from "react";
 
 const Notice = () => {
-  return (
-    <Text>
-      <span>현재 블로그 이전 중입니다.</span>
-      <br />
-      <a href="https://velog.io/@eunnbi">👉 전 블로그 보러가기</a>
-    </Text>
-  );
+  const [show, setShow] = useState(true);
+  return show ? (
+    <Box>
+      <p>현재 블로그 이전 중입니다.</p>
+      👉<a href="https://velog.io/@eunnbi"> 전 블로그 보러가기</a>
+      <button onClick={() => setShow(false)}>
+        <IoClose />
+      </button>
+    </Box>
+  ) : null;
 };
 
 export default Notice;
 
-const Text = styled.p`
-  margin-bottom: 20px;
+const Box = styled.div`
+  position: fixed;
+  top: 10px;
+  left: 50%;
+  transform: translateX(-50%);
   text-align: center;
   border-radius: 10px;
+  padding: 1rem;
+  z-index: 10;
+  width: 60vw;
+  max-width: 250px;
+  background-color: ${({ theme }) => theme.color.tabSelectedBgColor};
   a:hover {
     text-decoration: underline;
+  }
+  button {
+    color: ${({ theme }) => theme.color.textColor};
+    position: absolute;
+    top: 5px;
+    right: 5px;
+    font-size: 1.2rem;
+  }
+  @media ${({ theme }) => theme.device.tablet} {
   }
 `;
