6af18826e59be141bd788358c65cfd17126bf550
Eunbi <jenabill@naver.com>
2022-12-22 12:32:18 +0900

    refactor: MyInfoSection component

--- components/about/myinfo/MyInfoSection.tsx
+++ components/about/myinfo/MyInfoSection.tsx
@@ -1,19 +1,17 @@
 import styled from "styled-components";
 import Image from "next/image";
 import MyInfoLinks from "./MyInfoLinks";
-import KeywordsRotatingText from "@components/about/keywords/KeywordsRotatingText";
+import KeywordsRotatingText from "@components/common/KeywordsRotatingText";
 
 const MyInfoSection = () => {
   return (
     <Section>
       <Image
         src="/images/profile.png"
-        placeholder="blur"
-        blurDataURL="/images/profile.png"
         alt="profile"
         width={100}
         height={100}
-        objectFit="contain"
+        priority
       />
       <KeywordsRotatingText fontSize="2rem" textAlign="center" />
       <MyInfoLinks />
@@ -28,4 +26,7 @@ const Section = styled.section`
   flex-direction: column;
   gap: 1rem;
   align-items: center;
+  img {
+    object-fit: contain;
+  }
 `;
