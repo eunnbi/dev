b05071909d3e3a057712eb8db59fba5ca21df6e5
Eunbi <jenabill@naver.com>
2022-12-23 14:53:23 +0900

    design: theme object

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,21 +1,21 @@
 const color = {
   lightPurple: "#dcc9fc",
   darkPurple: "#6b4ba3",
-  purple: "#936aa7",
+  purple: "#936aa7"
 };
 
 const deviceSizes = {
   smallMobile: "300px",
   mobile: "500px",
   tablet: "900px",
-  laptop: "1050px",
+  laptop: "1050px"
 };
 
 const device = {
   mobile: `screen and (max-width: ${deviceSizes.mobile})`,
   tablet: `screen and (max-width: ${deviceSizes.tablet})`,
   laptop: `screen and (max-width: ${deviceSizes.laptop})`,
-  smallMobile: `screen and (max-width: ${deviceSizes.smallMobile})`,
+  smallMobile: `screen and (max-width: ${deviceSizes.smallMobile})`
 };
 
 export const lightTheme = {
@@ -23,12 +23,12 @@ export const lightTheme = {
     bgColor: "#fff",
     textColor: "#000",
     shadowColor: "rgba(0, 0, 0, 0.5)",
-    tabSelectedBgColor: "rgba(13, 12, 34, 0.05)",
+    tabSelectedBgColor: "#ededed",
     chipBgColor: "#f3f3f4",
-    tabTextColor: "#6e6d7a",
+    tabTextColor: "#6e6d7a"
   },
   name: "light" as const,
-  device,
+  device
 };
 
 export const darkTheme = {
@@ -38,8 +38,8 @@ export const darkTheme = {
     shadowColor: "#000",
     tabSelectedBgColor: "#373e47",
     chipBgColor: "#323a42",
-    tabTextColor: "#97a2ad",
+    tabTextColor: "#97a2ad"
   },
   name: "dark" as const,
-  device,
+  device
 };
