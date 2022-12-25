2bdc48ddf89f6d8db6f1887d5c5b9b5cf13e9697
Eunbi <jenabill@naver.com>
2022-12-25 14:24:09 +0900

    🔧 update lang attribute

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ["ko"],
+    defaultLocale: "ko"
+  },
   compiler: {
     styledComponents: true,
     emotion: true
