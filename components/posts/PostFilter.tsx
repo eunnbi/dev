793fa0f41af1ae906a67190e81d7689a3cde5655
Eunbi <jenabill@naver.com>
2023-02-11 14:28:18 +0900

    refactor post filter component

--- components/posts/PostFilter.tsx
+++ components/posts/PostFilter.tsx
@@ -1,14 +1,12 @@
 import { useContext } from "react";
 import { CategoriesContext } from "@contexts/posts/CategoriesContext";
 import Filter from "@components/common/Filter";
-import { useSetRecoilState } from "recoil";
+import { useRecoilState } from "recoil";
 import { postFilterState } from "@stores/postFilterState";
-import { usePostFilter } from "@hooks/useFilter";
 
 const PostFilter = () => {
   const categories = useContext(CategoriesContext);
-  const filter = usePostFilter();
-  const setFilter = useSetRecoilState(postFilterState);
+  const [filter, setFilter] = useRecoilState(postFilterState);
   return <Filter filters={categories} filter={filter} setFilter={setFilter} />;
 };
 
