5a5dea685b789b7c7aaebd4f447f1bc60237b970
Eunbi <jenabill@naver.com>
2023-02-12 21:23:55 +0900

    feat: add react query setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,43 @@
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import { useState } from "react";
 import Header from "@components/layout/Header";
 import ThemeButton from "@components/layout/ThemeButton";
 import TopButton from "@components/layout/TopButton";
 import CustomThemeProvider from "@styles/CustomThemeProvider";
+import {
+  QueryClientProvider,
+  Hydrate,
+  QueryClient
+} from "@tanstack/react-query";
 import "@styles/globals.css";
 import "@styles/markdown.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            staleTime: Infinity
+          }
+        }
+      })
+  );
   return (
-    <RecoilRoot>
-      <CustomThemeProvider>
-        <Header />
-        <TopButton />
-        <ThemeButton />
-        <Component {...pageProps} />
-      </CustomThemeProvider>
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <RecoilRoot>
+          <CustomThemeProvider>
+            <Header />
+            <TopButton />
+            <ThemeButton />
+            <Component {...pageProps} />
+          </CustomThemeProvider>
+        </RecoilRoot>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
 
